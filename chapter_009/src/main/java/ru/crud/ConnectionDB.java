464bd167b69aae7e2d4d9887420239f68d6185c6
nik <bespalov-ns@mail.ru>
2017-09-11 09:43:08 +0300

    1. Crud servlet [#2512] Connection pool added.

--- chapter_009/src/main/java/ru/crud/ConnectionDB.java
+++ chapter_009/src/main/java/ru/crud/ConnectionDB.java
@@ -1,12 +1,12 @@
 package ru.crud;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -15,7 +15,10 @@ import java.util.Properties;
  * Created by nikolay on 30/08/17.
  */
 public class ConnectionDB {
-
+    /**
+     * Connection pool.
+     */
+    private BasicDataSource connectionPool;
     /**
      * Logger for DB.
      */
@@ -24,14 +27,35 @@ public class ConnectionDB {
      * Constructor with db creation.
      */
     public ConnectionDB() {
+        connectionPool = new BasicDataSource();
+        init();
         createDB();
     }
     /**
      * Connection getter.
      * @return connection to db.
+     * @throws SQLException - ex.
      */
-    public Connection getConnection() {
+    public Connection getConnection() throws SQLException {
+        return this.connectionPool.getConnection();
+    }
 
+    /**
+     * Create DB structure.
+     */
+    private void createDB() {
+        String createDB = "create table if not exists users (name varchar(254), login varchar(254) primary key, email varchar(254), createDate bigint);";
+        try (Connection connection = this.connectionPool.getConnection()) {
+            PreparedStatement stat = connection.prepareStatement(createDB);
+            stat.executeUpdate();
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
+    }
+    /**
+     * Pool initialization.
+     */
+    private void init() {
         Properties props = new Properties();
         try {
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
@@ -41,25 +65,12 @@ public class ConnectionDB {
             String url = props.getProperty("jdbc.url");
             String username = props.getProperty("jdbc.username");
             String password = props.getProperty("jdbc.password");
-            DriverManager.registerDriver(new org.postgresql.Driver());
-            return DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-        return null;
-    }
-
-    /**
-     * Create DB structure.
-     */
-    private void createDB() {
-        String createDB = "create table if not exists users (name varchar(254), login varchar(254) primary key, email varchar(254), createDate bigint);";
-        try (Connection connection = this.getConnection()) {
-            PreparedStatement stat = connection.prepareStatement(createDB);
-            stat.executeUpdate();
-        } catch (SQLException e) {
+            this.connectionPool.setUsername(username);
+            this.connectionPool.setPassword(password);
+            this.connectionPool.setDriverClassName("org.postgresql.Driver");
+            this.connectionPool.setUrl(url);
+            this.connectionPool.setInitialSize(5);
+        }  catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
